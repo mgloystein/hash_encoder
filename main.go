5de87aef4ea54fc68ade1c20b3f1bd3d42bbacb8
Michael Gloystein <mgloystein@gmail.com>
2020-09-30 13:35:19 -0600

    Adding help

--- main.go
+++ main.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/mgloystein/hash_encoder/config"
 	"github.com/mgloystein/hash_encoder/server"
 )
 
 func main() {
+	args := os.Args[1:]
+	if len(args) > 0 {
+		for _, arg := range args {
+			if arg == "--help" {
+				fmt.Println(`
+hash-server [options]
+	options:
+	  -p   Sets the server port          (default 8080)
+	  -w   Sets the service worker count (default 10)
+	  -d   Sets the write delay          (default 5 seconds)
+	  -S   Sets the hashing secret
+
+
+hash-server usage:
+
+    hash-server -p 8080 -w 4 -d 10
+				`)
+				return
+			}
+		}
+	}
+
 	c := config.DefaultConfig()
 	handler, err := server.NewHashEncoder(c)
 
